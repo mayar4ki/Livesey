d9bf7b712d2abc6392bb3fbefe19d3dcee630139
Mayar Deeb <mayar.e.deeb@gmail.com>
2025-11-09 00:50:43 +0400

    refactor: update wagmi configuration and add confirmation block count

--- apps/admin-app/src/_config/wagmi.ts
+++ apps/admin-app/src/_config/wagmi.ts
@@ -1,15 +1,12 @@
 import { cookieStorage, createConfig, createStorage, http } from 'wagmi';
 import { sepolia } from 'wagmi/chains';
 
-import { metaMask } from 'wagmi/connectors';
-
 export const getConfig = () => {
   return createConfig({
     chains: [
       // mainnet,
       sepolia,
     ],
-    connectors: [metaMask()],
     transports: {
       //[mainnet.id]: http(`https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`),
       [sepolia.id]: http(`https://eth-sepolia.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`),
@@ -20,3 +17,6 @@ export const getConfig = () => {
     }),
   });
 };
+
+// will be used to check if the transaction is confirmed
+export const CONFIRMATION_BLOCK_COUNT = 12;
