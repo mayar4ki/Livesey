3ff2290ca2ae0e82b51be6b771417aa94bdd0e1e
Mayar Deeb <mayar.e.deeb@gmail.com>
2025-11-09 00:48:23 +0400

    refactor: 更新 Template 组件中的动画过渡效果

--- apps/admin-app/src/app/template.tsx
+++ apps/admin-app/src/app/template.tsx
@@ -12,8 +12,8 @@ export default function Template({ children }: { children: React.ReactNode }) {
         key={pathname}
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: -20 }}
-        transition={{ duration: 0.3, ease: 'easeInOut' }}
+        // exit={{ opacity: 1, y: -20 }}
+        transition={{ duration: 0.3, ease: 'linear' }}
         className=" flex-1"
       >
         {children}
